19d422cc21fd9613e8a35af0f206e8576a735dcb
TrevorZylks <trevorzylks@gmail.com>
2021-06-26 15:34:08 -0700

    commit tree 59104f402dac863e345ed70dd4eb8cf9d42f0c78

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'Trevor';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let firstInitial = currentUser.slice[0];
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
