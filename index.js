7500a88f9f76ae2fe1de12143e3059cc67cea4b4
TrevorZylks <trevorzylks@gmail.com>
2021-06-26 15:34:33 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Trevor';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
